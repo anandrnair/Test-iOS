4917bef96399546548cb859dd6d7e59733cf5333
Anand R Nair <wolf547@gmail.com>
2018-06-17 23:45:48 +0530

    implement pull to refresh

--- iOS  Proficiency Test/iOS  Proficiency Test/View Controllers/PlaceViewController.swift
+++ iOS  Proficiency Test/iOS  Proficiency Test/View Controllers/PlaceViewController.swift
@@ -10,19 +10,46 @@ import UIKit
 
 class PlaceViewController: UITableViewController {
     
-    var place: Place?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        getPlaceInformation()
-    }
-
-    func getPlaceInformation() {
-        Place.getPlaceDetails { (place, error) in
-            
+    let refreshController = UIRefreshControl()
+    
+    var place: Place? {
+        didSet {
+            onmain {
+                self.title = self.place?.title ?? ""
+                self.tableView.reloadData()
+            }
         }
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if #available(iOS 10.0, *) {
+            tableView.refreshControl = refreshController
+        } else {
+            tableView.addSubview(refreshController)
+        }
+        
+        refreshController.addTarget(self, action: #selector(refreshData(_:)), for: .valueChanged)
+
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = UITableViewAutomaticDimension
+        getPlaceInformation()
+    }
+
+    @objc private func refreshData(_ sender: Any) {
+        getPlaceInformation(shouldShowHud: false)
+    }
+    
+    func getPlaceInformation(shouldShowHud: Bool = true) {
+        Place.getPlaceDetails(shouldShowProgressHud: shouldShowHud) { (place, error) in
+            self.refreshController.endRefreshing()
+            guard error == nil else {
+                return
+            }
+            self.place = place
+        }
+    }
 }
 
 extension PlaceViewController {
